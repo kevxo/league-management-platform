d94cbb3ae47cd9e52a7f7b1802ff31473b6fb554
Kevin Cuadros <kev040798@gmail.com>
2026-04-04 15:13:33 -0400

    Create protected route

---
+++ league-manager/src/Components/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }: { children: React.ReactElement }) => {
+    const user = localStorage.getItem("user");
+
+    if (!user) {
+        return <Navigate to="/login" />
+    }
+
+    return children;
+}
+
+
+export default ProtectedRoute;
